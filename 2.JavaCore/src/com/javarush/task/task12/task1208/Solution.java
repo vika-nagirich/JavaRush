d6ae17c40942d03dfa22f75f2c119fbb02b712ca
Vika <vikaproshinoka@gmail.com>
2024-04-06 21:15:05 +0300

    done 4 lecture JavaCore

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void print(){}
+    public static void print(int a){}
+    public static void print(int a, int b){}
+    public static void print(String a){}
+    public static void print(double a){}
+    //Напишите тут ваши методы
+}
