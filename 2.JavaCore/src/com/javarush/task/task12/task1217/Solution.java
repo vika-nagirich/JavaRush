5025d653f0a8955e0265b005a2ef1808f74a8581
Vika <vikaproshinoka@gmail.com>
2024-06-27 16:24:31 +0300

    done 12 lecture in JavaCore

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    interface CanFly{
+        void fly();
+    }
+    interface CanRun{
+        boolean run();
+    }
+    interface CanSwim{
+        int swim();
+    }
+
+
+}
